72876cc9de618645b100c90fa2a6dc67fd149203
Reema Alzaid <80041251+ReemaAlzaid@users.noreply.github.com>
2022-10-07 16:55:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, jsonify, request
 import json
-import time
+
+#declared an empty variable for reassignment
+response = ''
 
 #creating the instance of our flask application
-app = Flask(__name__)
-@app.toute("/bot", method =["POST"])
+app = Flask(_name_)
 
-def response():
-    query = dict(request.form)['query']
-    result = query + " " + time.ctime()
-    return jsonify({"response": result})
+#route to entertain our post and get request from flutter app
+@app.route('/name', methods = ['GET', 'POST'])
+def nameRoute():
 
+    #fetching the global response variable to manipulate inside the function
+    global response
 
+    #checking the request type we get from the app
+    if(request.method == 'POST'):
+        request_data = request.data #getting the response data
+        request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
+        name = request_data['name'] #assigning it to name
+        response = f'Hi {name}! this is Python' #re-assigning response with the name we got from the user
+        return " " #to avoid a type error 
+    else:
+        return jsonify({'name' : response}) #sending data back to your frontend app
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",)
-
+if _name_ == "_main_":
+    app.run(debug=True)
